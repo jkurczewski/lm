55615afd6c1f21cbf363d99424b4c848eb7ed56a
Kuba <jkurczewski@edu.cdv.pl>
2021-06-05 14:09:05 +0200

    Settings view boilerplate fix

--- resources/views/ustawienia.blade.php
+++ resources/views/ustawienia.blade.php
@@ -20,15 +20,6 @@
                 <div class="col-12 mt-4">
                     <div class="card">
                         <div class="card-body pb-0 pt-1">
-                            <h5 class="col-12 mt-2 card-title fs-4 mt-2"><b>Zresetuj konto</b></h5>
-                            <hr/>
-
-                          </div>
-                    </div>
-                </div>
-                <div class="col-12 mt-4">
-                    <div class="card">
-                        <div class="card-body pb-0 pt-1">
                             <h5 class="col-12 mt-2 card-title fs-4 mt-2"><b>Usuń konto</b></h5>
                             <hr/>
 
