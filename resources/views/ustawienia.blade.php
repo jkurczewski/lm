3d56f1d0d0b2b3c4d8143ac14590809ba4a6ddd8
Kuba <jkurczewski@edu.cdv.pl>
2021-06-05 13:38:07 +0200

    Settings view boilerplate

--- resources/views/ustawienia.blade.php
+++ resources/views/ustawienia.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row d-flex">
+        <div class="col-12 col-md-12 header mt-4 mb-4">
+           <h1>Ustawienia</h1>
+        </div>
+        <div class="col-12 mt-4">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body pb-0 pt-1">
+                            <h5 class="col-12 mt-2 card-title fs-4 mt-2"><b>Zmień hasło</b></h5>
+                            <hr/>
 
+                          </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-4">
+                    <div class="card">
+                        <div class="card-body pb-0 pt-1">
+                            <h5 class="col-12 mt-2 card-title fs-4 mt-2"><b>Zresetuj konto</b></h5>
+                            <hr/>
+
+                          </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-4">
+                    <div class="card">
+                        <div class="card-body pb-0 pt-1">
+                            <h5 class="col-12 mt-2 card-title fs-4 mt-2"><b>Usuń konto</b></h5>
+                            <hr/>
+
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
